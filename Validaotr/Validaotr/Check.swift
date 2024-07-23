d8fdbf62c089278bdd6f2b3bcc1bc2300e7fdfdd
Haroon <haroon5507@gmail.com>
2024-07-23 10:47:07 +0500

    firstcommit

---
+++ Validaotr/Validaotr/Check.swift
@@ -0,0 +1,16 @@
+//
+//  Check.swift
+//  Validaotr
+//
+//  Created by Haroon Shoukat on 23/07/2024.
+//
+
+import Foundation
+
+public class Check {
+    public init() {}
+    
+    public func checkStatus(){
+        print("@@@@00000 test")
+    }
+}
---
+++ Validaotr/Validaotr/Validaotr.h
@@ -0,0 +1,18 @@
+//
+//  Validaotr.h
+//  Validaotr
+//
+//  Created by Haroon Shoukat on 23/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Validaotr.
+FOUNDATION_EXPORT double ValidaotrVersionNumber;
+
+//! Project version string for Validaotr.
+FOUNDATION_EXPORT const unsigned char ValidaotrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Validaotr/PublicHeader.h>
+
+
